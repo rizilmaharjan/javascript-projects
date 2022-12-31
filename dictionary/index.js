31ca48b8274ca97134dc4730c348029fc0d6ee55
rizilmaharjan <rizilmaharjan@gmail.com>
2022-12-31 12:26:19 +0545

    dictionary changed

--- dictionary/index.js
+++ dictionary/index.js
@@ -1,29 +1,31 @@
 const options = {
-	method: "GET",
+	method: 'GET',
 	headers: {
 		'X-RapidAPI-Key': 'b4a4353689msh9a63c61d5b36c90p176047jsnaf301a4efec5',
-		'X-RapidAPI-Host': 'dictionary-by-api-ninjas.p.rapidapi.com',
-	},
+		'X-RapidAPI-Host': 'mashape-community-urban-dictionary.p.rapidapi.com'
+	}
 };
-
 
 const dict = (inp_word)=>{
 
-    fetch('https://dictionary-by-api-ninjas.p.rapidapi.com/v1/dictionary?word=' + inp_word, options)
+	fetch('https://mashape-community-urban-dictionary.p.rapidapi.com/define?term=' + inp_word, options)
 	.then(response => response.json())
 	.then(response => {
+		word.innerHTML = response.list[2].word;
+		meaning1.innerHTML= response.list[1].definition;
+		meaning2.innerHTML= response.list[2].definition;
+		meaning3.innerHTML= response.list[3].definition;
 
-    word.innerHTML = response.word;
-        meaning.innerHTML = response.definition;
-        console.log(response)
- })
-        
+		example1.innerHTML= response.list[3].example;
+		example2.innerHTML= response.list[4].example;
+
+		
+		})
 	.catch(err => console.error(err));
+
 }
-
-dict("welcome");
-
-my_button.addEventListener("click",(event) =>{
-    event.preventDefault();
-    dict(inp.value)
+dict("welcome")
+my_button.addEventListener("click",(e)=>{
+	e.preventDefault();
+	dict(inp.value);
 })
